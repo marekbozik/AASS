baf620942edbbb6c70444d51912820934b151fb9
David Petricko <dpetricko@gmail.com>
2023-04-01 16:54:41 +0200

    Deleting old files

--- SOA/service1/src/server.ts
+++ SOA/service1/src/server.ts
@@ -1,21 +1,10 @@
 import express, { Request, Response } from 'express';
-import { UserController } from './userController';
-import { UserService } from './userService';
 
 const app = express();
-const userService = new UserService();
-const userController = new UserController(userService);
 
 app.use(express.json());
 
-app.post('/users', (req: Request, res: Response) => {
-});
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
-  userController.registerUser().then(() => {
-    console.log('User created');
-  }).catch((err) => {
-      console.error(err);
-  });
 });
