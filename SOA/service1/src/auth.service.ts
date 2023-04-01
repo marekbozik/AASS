8672c47c2e913a27bab6fad5e5eb63ce0b45cf53
David Petricko <dpetricko@gmail.com>
2023-04-01 16:55:38 +0200

    Added authentification service

---
+++ SOA/service1/src/auth.service.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { comparePassword, hashPassword } from './utils';
+
+
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn'],
+  });
+
+export class AuthService {
+    async registerUser(request: Request, response: Response) {
+        const { firstname, lastname, email, password, isTeacher } = request.body;
+
+        try {
+            const existingUser = await prisma.user.findUnique({
+                where: {
+                    Email: email
+                }
+            });
+    
+            if (existingUser) {
+                return response.status(409).json({ message: 'User already exists' });
+            }
+    
+            const passwordHash = await hashPassword(password);
+    
+            await prisma.user.create({
+                data: {
+                    FirstName: firstname,
+                    LastName: lastname,
+                    Email: email,
+                    PasswordHash: passwordHash,
+                    IsTeacher: isTeacher
+                }
+            });
+    
+            return await response.status(201).json({ message: 'User created' });
+        }
+        catch (err) {
+            console.error(err);
+            return response.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    async loginUser(request: Request, response: Response) {
+        const { email, password } = request.body;
+
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    Email: email
+                }
+            });
+    
+            if (!user) {
+                return response.status(404).json({ message: 'User not found' });
+            }
+    
+            if (!comparePassword(password, user.PasswordHash)) {
+                return response.status(401).json({ message: 'Invalid password' });
+            }
+    
+            return response.status(200).json({ message: 'User logged in' });
+        } catch (err) {
+            console.error(err);
+            return response.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+}
--- SOA/service1/src/server.ts
+++ SOA/service1/src/server.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
+import { AuthService } from './auth.service';
 
 const app = express();
+const authService = new AuthService();
 
 app.use(express.json());
 
+app.post('/register', (req: Request, res: Response) => {
+  authService.registerUser(req, res);
+});
+
+app.post('/login', (req: Request, res: Response) => {
+  authService.loginUser(req, res);
+});
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
